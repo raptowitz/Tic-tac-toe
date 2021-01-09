48f753abfadf8a5c0ea84d3eaf4a38e581359c66
Rachael Aptowitz <raptowitz@gmail.com>
2021-01-08 18:31:12 -0800

    separating into different objects and check victory

--- game.rb
+++ game.rb
@@ -3,45 +3,58 @@
 # Gameboard for tic-tac-toe
 class Board
   def initialize
-    @empty_board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    @board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
   end
 
   def print_board
-    @empty_board.each do |row|
+    @board.each do |row|
       puts row.join('|')
-    end
-  end
-
-  def player1_move
-    puts 'Player 1, what is your move?'
-    @space = gets.to_i
-
-    if @space.between?(1, 3)
-      @empty_board[0][@space - 1] = 'X'
-    elsif @space.between?(4, 6)
-      @empty_board[1][@space - 4] = 'X'
-    elsif @space.between?(7, 9)
-      @empty_board[2][@space - 7] = 'X'
-    end
-  end
-
-  def player2_move
-    puts 'Player 2, what is your move?'
-    @space = gets.to_i - 1
-
-    if @space.between?(1, 3)
-      @empty_board[0][@space - 1] = 'O'
-    elsif @space.between?(4, 6)
-      @empty_board[1][@space - 4] = 'O'
-    elsif @space.between?(7, 9)
-      @empty_board[2][@space - 7] = 'O'
     end
   end
 end
 
-new_board = Board.new
-new_board.print_board
-new_board.player1_move
-new_board.print_board
-new_board.player2_move
-new_board.print_board
+# Player moves
+class Player < Board
+  def player_move(player)
+    @player = player
+    print_board
+    puts "Player #{@player}, what is your move?"
+    @space = gets.to_i
+
+    if @space.between?(1, 3)
+      @board[0][@space - 1] = @player
+    elsif @space.between?(4, 6)
+      @board[1][@space - 4] = @player
+    elsif @space.between?(7, 9)
+      @board[2][@space - 7] = @player
+    end
+  end
+end
+
+# Check victory
+class Score < Player
+  @victory = false
+
+  def play_game
+    until @victory
+      player_move('X')
+      check_victory
+      player_move('O')
+      check_victory
+    end
+  end
+
+  def check_victory
+    i = 0
+    while i < 3 do
+      if @board[i][0] == @board[i][1] && @board[i][0] == @board[i][2]
+        @victory = true
+        puts 'Victory'
+      end
+      i += 1
+    end
+  end
+end
+
+new_game = Score.new
+new_game.play_game
